3203ea8c2e3a15f558699dcc6ea475c3bca9f910
Heitor Hugo <bentoheitor4@gmail.com>
2024-07-01 10:01:33 -0300

    Update and rename 33_[Python]_Introd02.py to Ex_33[Python].py

--- Ex_33[Python].py
+++ Ex_33[Python].py
@@ -1,15 +1,12 @@
-'''
-Faça um programa que receba uma medida em pés.
-Faça as conversões a seguir em mostre os resultados.
-    a) polegadas
-    b) jardas
-    c) milhas
-Sabese que:
-    1 pé = 12 polegadas
-    1 jarda = 3 pés
-    1 milha = 1760 jardas
+Faça um programa que receba uma medida em pés. Faça as conversões a seguir em mostre os resultados.
+a) polegadas
+b) jardas
+c) milhas
 
-'''
+Sabese que:
+1 pé = 12 polegadas
+1 jarda = 3 pés
+1 milha = 1760 jardas
 
 #variaveis
 pes=0.0 
