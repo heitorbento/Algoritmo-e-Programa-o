5a27c6309fc08fa3288dca74b66e0e2733937e91
Heitor Hugo <bentoheitor4@gmail.com>
2024-07-01 10:08:52 -0300

    Update and rename 38_[Python]_IF_Comando01.py to Ex_38[Python].py

--- Ex_38[Python].py
+++ Ex_38[Python].py
@@ -1,10 +1,7 @@
-'''
 Faça um algoritmo que receba o nome e duas notas de um aluno.
 Calcule a média e ao final mostre o nome do aluno, a média e a sua situação.
 Caso a média seja no mínimo 6, o aluno está aprovado, se a média for inferior a 6 e 
 no mínimo 4 o aluno está de Exame e se a média for inferior a 4 o mesmo está reprovado.
-
-'''
 
 #variaveis
 nome = ""
