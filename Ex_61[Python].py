b2b1b1fb3db8ce4615da948a9d69287c93fa3db2
Heitor Hugo <bentoheitor4@gmail.com>
2024-07-01 10:25:41 -0300

    Update and rename 61_[Python]_Funcao_Comando02.py to Ex_61[Python].py

--- Ex_61[Python].py
+++ Ex_61[Python].py
@@ -10,17 +10,6 @@
 # Para um n informado pelo usuário. Use uma função que receba um valor n 
 # inteiro e imprima até a n-ésima linha.
 
-
-
-
-
-
-
-
-
-
-
-
 def num_cascate1(n):
     for x in range(1,n+1):
         print(end ='\n')
